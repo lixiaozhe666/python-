f2d62891ed09669ab12e8bb783b1d2d6dc693239
lixiaozhe666 <15926470603@163.com>
2018-05-13 09:32:08 +0800

    selenium创建，打开与关闭

---
+++ demoSelenium.py
@@ -0,0 +1,19 @@
+#!usr/bin/env python  
+#-*- coding:utf-8 _*-  
+""" 
+@author:lz 
+@file: demoSelenium.py 
+@time: 2018/05/12   10：21 
+"""
+from selenium import webdriver
+import time
+#指定浏览器驱动位置
+dirver_path = r'/home/lz/Document/python/pythonCourseDemo/chromedriver'
+dirver =webdriver.Chrome(executable_path=dirver_path)
+dirver.get("https://www.baidu.com")
+
+time.sleep(5)
+dirver.close()#关闭当前选项卡
+
+time.sleep(3)
+dirver.quit()#退出整个浏览器
