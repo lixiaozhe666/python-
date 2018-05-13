82b6807eb867f3f4b9cb0c819132452e19aa69c6
lixiaozhe666 <15926470603@163.com>
2018-05-13 10:16:30 +0800

    selenium查找元素

--- demoSeleniumFindElement.py
+++ demoSeleniumFindElement.py
@@ -6,6 +6,7 @@
 @time: 2018/05/13   09：33 
 """
 from  selenium import webdriver
+from selenium.webdriver.support.select import By
 from lxml import etree
 
 driver_path = r"/home/lz/Document/python/pythonCourseDemo/chromedriver"
@@ -27,14 +28,17 @@ driver.get("https://www.baidu.com")
 # inputTag = driver.find_element_by_xpath("//input[@id='kw']")
 # inputTag.send_keys("python\n")
 
+# inputTag = driver.find_element(By.CLASS_NAME,"s_ipt")
+# inputTag.send_keys("python")
+
 # find_element:不带s说明查找出的是第一个符合要求的元素
 # find_elements:查找所有符合要求的元素，返回一个列表
 # inputTag= driver.find_elements_by_xpath("//input[@id='kw']")[0]
 # inputTag.send_keys("python\n")
 
 #当只需要解析网页时，要使用python解析库，lxml或者BeautifulSoup去解析，因为效率高
-html = etree.HTML(driver.page_source)
-inputTag = html.xpath("//input[@id='kw']")[0]
+# html = etree.HTML(driver.page_source)
+# inputTag = html.xpath("//input[@id='kw']")[0]
 #如果是需要点击网页的一个按钮或者输入数据，则需要使用selenium查找方法来完成查找
 
 
